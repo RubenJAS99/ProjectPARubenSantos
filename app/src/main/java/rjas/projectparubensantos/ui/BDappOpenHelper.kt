388881ff07fbd676aab5a2e46f3c22d67a4e2656
RubenJAS99 <rubensantos995@hotmail.com>
2022-06-12 14:07:38 +0100

    BDappOpenHelper creation.

---
+++ app/src/main/java/rjas/projectparubensantos/ui/BDappOpenHelper.kt
@@ -0,0 +1,23 @@
+package rjas.projectparubensantos.ui
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BDappOpenHelper(context: Context?) : SQLiteOpenHelper(context, NAME, null, VERSION) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        requireNotNull(db)
+
+        //UserTableBD(db).cria()
+        //FoodTableBD(db).cria()
+        //ProgressTableBD(db).cria()
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    }
+    companion object {
+        const val NAME = "App.db"
+        private const val VERSION = 1
+    }
+
+}
