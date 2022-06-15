592a8a476c953e976625330c6fab9095e870a30d
rubensantos <rubensantos995@hotmail.com>
2022-06-15 11:35:38 +0100

    User class created. Function to convert user into content values.

--- app/src/main/java/rjas/projectparubensantos/User.kt
+++ app/src/main/java/rjas/projectparubensantos/User.kt
@@ -3,11 +3,11 @@ package rjas.projectparubensantos
 import android.content.ContentValues
 import android.provider.BaseColumns
 
-data class user(var id: Long, var nome: String) {
+data class user(var id: Long, var name: String) {
 
     fun toContentValues() : ContentValues {
         val values = ContentValues()
-        values.put(UserTableBD.USER_NAME, nome)
+        values.put(UserTableBD.USER_NAME, name)
 
         return values
     }
