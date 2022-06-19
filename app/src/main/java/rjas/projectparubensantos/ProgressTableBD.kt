74c1d2542c1bfe0aae8a9f7f33c8c41c147d52b4
RubenJAS99 <rubensantos995@hotmail.com>
2022-06-19 19:51:29 +0100

    Progress class created.

--- app/src/main/java/rjas/projectparubensantos/ProgressTableBD.kt
+++ app/src/main/java/rjas/projectparubensantos/ProgressTableBD.kt
@@ -6,16 +6,16 @@ import android.provider.BaseColumns
 class ProgressTableBD(db: SQLiteDatabase) : BDTable(db, NAME) {
     override fun create() {
         db.execSQL("CREATE TABLE $name (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "$PROGRESS_DATE TEXT NOT NULL, $LAST_WEIGHT DOUBLE NOT NULL, $CURRENT_WEIGHT DOUBLE NOT NULL," +
+                "$PROGRESS_DATE DATE NOT NULL, $LAST_WEIGHT DOUBLE NOT NULL, $CURRENT_WEIGHT DOUBLE NOT NULL," +
                 "$USER_PERIOD TEXT NOT NULL)")
     }
 
     companion object {
         const val NAME = "progress"
         const val PROGRESS_DATE = "date"
-        const val LAST_WEIGHT = "weight"
-        const val CURRENT_WEIGHT = "height"
-        const val USER_PERIOD = "period" ////????????
+        const val LAST_WEIGHT = "lastWeight"
+        const val CURRENT_WEIGHT = "currentWeight"
+        const val USER_PERIOD = "period"
 
         val ALL_COLUMNS = arrayOf(BaseColumns._ID, PROGRESS_DATE, LAST_WEIGHT, CURRENT_WEIGHT, USER_PERIOD)
     }
