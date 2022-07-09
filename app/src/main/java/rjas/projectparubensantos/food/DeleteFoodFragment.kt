d8bdbb8887b40f983efd7de57dbbfc79979b7018
RubenJAS99 <rubensantos995@hotmail.com>
2022-07-09 11:22:44 +0100

    Options cancel and deleted food implemented on DeleteFoodFragment.

--- app/src/main/java/rjas/projectparubensantos/food/DeleteFoodFragment.kt
+++ app/src/main/java/rjas/projectparubensantos/food/DeleteFoodFragment.kt
@@ -1,15 +1,21 @@
 package rjas.projectparubensantos.food
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import rjas.projectparubensantos.ContentProvider
 import rjas.projectparubensantos.MainActivity
 import rjas.projectparubensantos.R
 import rjas.projectparubensantos.databinding.FragmentDeleteFoodBinding
 import rjas.projectparubensantos.fragments.food.DeleteFoodArgs
+import rjas.projectparubensantos.fragments.food.DeleteFoodDirections
 
 class DeleteFoodFragment : Fragment() {
     private var _binding: FragmentDeleteFoodBinding? = null
@@ -52,9 +58,35 @@ class DeleteFoodFragment : Fragment() {
                 true
             }
             R.id.action_cancel -> {
-
+                goToFood()
                 true
             }
             else -> false
         }
+
+    private fun deleteFood() {
+        val foodAddress = Uri.withAppendedPath(ContentProvider.FOOD_ADDRESS, "${food.id}")
+        val deletedFoods = requireActivity().contentResolver.delete(foodAddress, null, null)
+
+        if (deletedFoods != 1) {
+            Snackbar.make(
+                binding.textViewName,
+                R.string.error_delete_food,
+                Snackbar.LENGTH_INDEFINITE
+            ).show()
+            return
+        }
+
+        Toast.makeText(requireContext(), R.string.success_delete_food, Toast.LENGTH_LONG).show()
+        goToFood()
+    }
+
+    private fun goToFood() {
+        val action = DeleteFoodDirections.actionNavDeleteFoodToNavFood()
+        findNavController().navigate(action)
+/*        private fun goToFood() {
+            findNavController().navigate(R.id.action_nav_delete_food_to_nav_food)
+        }*/
+    }
+
 }
