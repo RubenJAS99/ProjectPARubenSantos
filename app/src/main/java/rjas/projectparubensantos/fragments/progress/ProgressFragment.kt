2489cd24ce8b3830815821550450d0fb734b4d98
RubenJAS99 <rubensantos995@hotmail.com>
2022-07-15 16:57:11 +0100

    calendarview and textViewDateProgress added into fragment_progress. ProgressViewModel deleted. insertProgress function created. Returning the correct view inflater on ProgressFragment.

--- app/src/main/java/rjas/projectparubensantos/fragments/progress/ProgressFragment.kt
+++ app/src/main/java/rjas/projectparubensantos/fragments/progress/ProgressFragment.kt
@@ -8,12 +8,16 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.SimpleCursorAdapter
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import rjas.projectparubensantos.ContentProvider
 import rjas.projectparubensantos.MainActivity
 import rjas.projectparubensantos.R
 import rjas.projectparubensantos.databinding.FragmentProgressBinding
+import rjas.projectparubensantos.progress.Progress
 
 class ProgressFragment: Fragment() {
 
@@ -28,18 +32,10 @@ class ProgressFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val progressViewModel =
-            ViewModelProvider(this).get(ProgressViewModel::class.java)
-
         _binding = FragmentProgressBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-/*        val textView: TextView = binding.textProgress
-        progressViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }*/
         (activity as MainActivity).idMainMenu = R.menu.editing_menu
-        return root
+        return inflater.inflate(R.layout.fragment_progress, container, false)
     }
 
     override fun onDestroyView() {
@@ -50,11 +46,24 @@ class ProgressFragment: Fragment() {
     fun menuOptions(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_save -> {
-                //save()
+                //insertProgress()
                 true
             }
             else -> false
         }
+    }
+    private fun insertProgress(lastWeight: Double = 0.0, currentWeight: Double, period: String) {
+        val progress = Progress(lastWeight, currentWeight, period)
+
+        val insertProgressAddress = requireActivity().contentResolver.insert(ContentProvider.PROGRESS_ADDRESS, progress.toContentValues())
+
+
+        if (insertProgressAddress == null) {
+            Snackbar.make(binding.editTextWeightProgress, R.string.error_inserting_progress, Snackbar.LENGTH_INDEFINITE).show()
+            return
+        }
+
+        Toast.makeText(requireContext(), R.string.success_insert_progress, Toast.LENGTH_LONG).show()
     }
 
 }
